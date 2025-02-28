be8af01892558e56858b9c38f0542d3ca6ee948b
ShivmMakone <shivammakone@gmail.com>
2025-02-28 06:04:39 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,8 +1,7 @@
 try:
       with open('sample.txt', 'r') as file:
         print("Reading file content:")
-        # Print content line by line with line number
-        for i, line in enumerate(file, start=1):
+               for i, line in enumerate(file, start=1):
             print(f"Line {i}: {line.strip()}")
 
 except FileNotFoundError:
