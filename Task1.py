afb1c0bffeca95225df007d3b222e736e9774d1b
ShivmMakone <shivammakone@gmail.com>
2025-02-28 06:03:54 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,6 +1,5 @@
 try:
-    # Open and read the file
-    with open('sample.txt', 'r') as file:
+      with open('sample.txt', 'r') as file:
         print("Reading file content:")
         # Print content line by line with line number
         for i, line in enumerate(file, start=1):
